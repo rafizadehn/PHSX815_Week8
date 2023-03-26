a2ad5d97532b3106ee425ceff9d92201abedf52e
neema <rafizadehn@ku.edu>
2023-03-26 11:02:23 -0500

    Typos

--- Neyman_Construction.py
+++ Neyman_Construction.py
@@ -28,23 +28,23 @@ if __name__ == "__main__":
         print
         sys.exit(1)  
     
-	# simulates experiments for all true p values, and store the resulting distributions of observed counts
+    # simulates experiments for all true p values, and store the resulting distributions of observed counts
     observed_counts = []
     for true_p in true_p_values:
         counts = [coin_flip(true_p, Nmeas) for i in range(Nexp)]
         observed_counts.append(counts)
 
-	# then calculates the mean and standard deviation
+    # then calculates the mean and standard deviation
     mean_counts = [np.mean(observed_counts[i]) for i in range(len(true_p_values))]
     std_counts = [np.std(observed_counts[i]) for i in range(len(true_p_values))]
 
-	# then associates each  measured p value for each true p value
+    # then associates each  measured p value for each true p value
     measured_p_values = [mean_counts[i] / Nmeas for i in range(len(true_p_values))]
 
-	# the standard deviation of the measured p value
+    # the standard deviation of the measured p value
     std_measured_p_values = [std_counts[i] / Nmeas for i in range(len(true_p_values))]
 
-	# creates a 2D grid of true and measured p values
+    # creates a 2D grid of true and measured p values
     p_true, p_measured = np.meshgrid(true_p_values, measured_p_values)
     z = norm.pdf(p_measured, loc=p_true, scale=std_measured_p_values)
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     colors = [(0, 0, 1), (1, 0.5, 1), (1, 0, 0)]
     cmap = LinearSegmentedColormap.from_list('custom', colors)
 
-	# Neyman construction and the 2D heatmap of the normal distribution together
+    # Neyman construction and the 2D heatmap of the normal distribution together
     fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
     ax1.scatter(true_p_values, measured_p_values, s=10, alpha=0.5, color='blue')
     ax1.errorbar(true_p_values, measured_p_values, yerr=std_measured_p_values, fmt='none', color='blue', alpha=0.5)
